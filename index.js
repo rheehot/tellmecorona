5d17bfe1c3d5c88c6612862df444f165f2f03c1d
lark <121there@gmail.com>
2020-02-24 23:45:40 +0900

    Add moment.js package

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const moment = require("moment");
+moment.locale("ko");
+
 const bot = new (require("./bot.js"))();
 const requester = new (require("./requester"))();
 const parser = new (require("./parser"))();
@@ -5,20 +8,15 @@ const storage = new (require("./storage"))();
 
 let today = new Date();
 
-bot.sendMessageAdmin(
-  `${today.getFullYear()}-${today.getMonth()}-${today.getDate()} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()} Start.`
-);
+bot.sendMessageAdmin(moment().format("YYYY-MM-DD HH:mm:ss") + " Start.");
 
 requester.load().then(data => {
   let lastTimestamp = parseInt(storage.readUpdateDate());
   let currentTimestamp = parser.getUpdateDate(data).getTime();
 
   if (currentTimestamp !== lastTimestamp) {
-    let currentDate = new Date(currentTimestamp);
-    let ampm = currentDate.getHours() > 12 ? "오후" : "오전";
-    let hours =
-      ampm === "오후" ? currentDate.getHours() - 12 : currentDate.getHours();
-    let timeMessage = `${currentDate.getFullYear()}년 ${currentDate.getMonth()}월 ${currentDate.getDate()}일 ${ampm} ${hours}시\n`;
+    let currentDate = moment(currentTimestamp).subtract(1, "months");
+    let timeMessage = currentDate.format("YYYY년 MM월 DD일 A hh시") + "\n";
     let statusMessage = parser
       .getStatus(data)
       .map(arr => {
