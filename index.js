cc6fdc8b8cb1ab11c96c21910787498e9d3247e6
lark <121there@gmail.com>
2020-02-26 19:49:13 +0900

    file방식에서 mysql 방식으로 변경

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const moment = require("moment");
+const _ = require("lodash");
+const db = new (require("./database/mysql"))();
 moment.locale("ko");
 
 const bot = new (require("./bot.js"))();
 const requester = new (require("./requester"))();
 const parser = new (require("./parser"))();
-const storage = new (require("./storage"))();
-
-let today = new Date();
 
 bot.sendMessageAdmin(moment().format("YYYY-MM-DD HH:mm:ss") + " Start.");
 
 requester.load().then(data => {
-  let lastTimestamp = parseInt(storage.readUpdateDate());
   let currentTimestamp = parser.getUpdateDate(data).getTime();
 
-  if (currentTimestamp !== lastTimestamp) {
-    let currentDate = moment(currentTimestamp).subtract(1, "months");
-    let timeMessage = currentDate.format("YYYY년 MM월 DD일 A hh시") + "\n";
-    let statusMessage = parser
-      .getStatus(data)
-      .map(arr => {
-        return arr[0] + " " + arr[1] + "명";
-      })
-      .join("\n");
+  db.getRecentLog(recentLog => {
+    let lastTimestamp = new Date(recentLog.date).getTime();
 
-    console.log(timeMessage + statusMessage);
+    if (currentTimestamp !== lastTimestamp) {
+      let currentDate = moment(currentTimestamp);
+      let timeMessage = currentDate.format("YYYY년 MM월 DD일 A hh시") + "\n";
+      let status = parser.getStatus(data);
+      let statusMessage = status
+        .map(item => {
+          let result = item.data.title + " " + item.data.displayValue + "명";
 
-    bot.sendMessage(timeMessage + statusMessage);
-    storage.saveUpdateDate(currentTimestamp);
-  }
+          if (item.data.value > 0) {
+            result += `(+${item.data.value - recentLog[item.key]})`;
+          }
+
+          return result;
+        })
+        .join("\n");
+
+      let infectedItem = _.find(status, { key: "infected" });
+      let testedItem = _.find(status, { key: "tested" });
+      let recoveredItem = _.find(status, { key: "recovered" });
+      let deathsItem = _.find(status, { key: "deaths" });
+
+      db.addLog(
+        currentDate.format("YYYY-MM-DD HH:mm:ss"),
+        infectedItem.data.value,
+        recoveredItem.data.value,
+        deathsItem.data.value,
+        testedItem.data.value
+      );
+
+      console.log(timeMessage + statusMessage);
+      bot.sendMessage(timeMessage + statusMessage);
+    }
+  });
+
+  db.end();
 });
