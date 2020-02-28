5ba3e184d387f6f973ee2dc95d6679f1b5b119eb
lark <121there@gmail.com>
2020-02-28 18:51:30 +0900

    Add error handling

--- src/index.ts
+++ src/index.ts
@@ -9,53 +9,58 @@ const parser = new (require('./parser'))()
 
 bot.sendStartMessage()
 
-requester.load().then((data: any) => {
-  let currentTimestamp: number = parser.getUpdateDate(data).getTime()
+requester
+  .load()
+  .then((data: any) => {
+    let currentTimestamp: number = parser.getUpdateDate(data).getTime()
 
-  db.getRecentLog((recentLog: any) => {
-    let lastTimestamp: number = new Date(recentLog.date).getTime()
+    db.getRecentLog((recentLog: any) => {
+      let lastTimestamp: number = new Date(recentLog.date).getTime()
 
-    console.log(currentTimestamp, lastTimestamp)
+      console.log(currentTimestamp, lastTimestamp)
 
-    if (currentTimestamp !== lastTimestamp) {
-      let currentDate: any = moment(currentTimestamp)
-      let timeMessage: string =
-        currentDate.format('YYYY년 MM월 DD일 A hh시') + '\n'
-      let status: object[] | null = parser.getStatus(data)
+      if (currentTimestamp !== lastTimestamp) {
+        let currentDate: any = moment(currentTimestamp)
+        let timeMessage: string =
+          currentDate.format('YYYY년 MM월 DD일 A hh시') + '\n'
+        let status: object[] | null = parser.getStatus(data)
 
-      if (status !== null) {
-        let statusMessage: string = status
-          .map((item: any) => {
-            let result: string =
-              item.data.title + ' ' + item.data.displayValue + '명'
-            let increment: number = item.data.value - recentLog[item.key]
+        if (status !== null) {
+          let statusMessage: string = status
+            .map((item: any) => {
+              let result: string =
+                item.data.title + ' ' + item.data.displayValue + '명'
+              let increment: number = item.data.value - recentLog[item.key]
 
-            if (increment > 0) {
-              result += `(+${increment})`
-            }
+              if (increment > 0) {
+                result += `(+${increment})`
+              }
 
-            return result
-          })
-          .join('\n')
+              return result
+            })
+            .join('\n')
 
-        let infectedItem: any = _.find(status, { key: 'infected' })
-        let testedItem: any = _.find(status, { key: 'tested' })
-        let recoveredItem: any = _.find(status, { key: 'recovered' })
-        let deathsItem: any = _.find(status, { key: 'deaths' })
+          let infectedItem: any = _.find(status, { key: 'infected' })
+          let testedItem: any = _.find(status, { key: 'tested' })
+          let recoveredItem: any = _.find(status, { key: 'recovered' })
+          let deathsItem: any = _.find(status, { key: 'deaths' })
 
-        db.addLog(
-          currentDate.format('YYYY-MM-DD HH:mm:ss'),
-          infectedItem.data.value,
-          recoveredItem.data.value,
-          deathsItem.data.value,
-          testedItem.data.value
-        )
+          db.addLog(
+            currentDate.format('YYYY-MM-DD HH:mm:ss'),
+            infectedItem.data.value,
+            recoveredItem.data.value,
+            deathsItem.data.value,
+            testedItem.data.value
+          )
 
-        console.log(timeMessage + statusMessage)
-        bot.sendMessage(timeMessage + statusMessage)
+          console.log(timeMessage + statusMessage)
+          bot.sendMessage(timeMessage + statusMessage)
+        }
       }
-    }
 
-    db.end()
+      db.end()
+    })
   })
-})
+  .catch((error: any) => {
+    bot.sendMessageAdmin('ERROR: ' + error.message + '(path: requester.load)')
+  })
--- src/requester.ts
+++ src/requester.ts
@@ -1,17 +1,19 @@
-;(function() {
+;(() => {
   const request = require('request')
 
   class requester {
-    public load() {
+    public load(): Promise<PromiseConstructor> {
       const url: string =
         'https://m.search.naver.com/search.naver?ie=utf8&query=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%99%95%EC%A7%84%EC%9E%90'
 
       return new Promise((resolve, reject) => {
         request(url, (error: any, request: any, body: any) => {
-          if (error && request.statusCode !== 200) {
+          if (error) {
             console.error('Failed: ', error)
-            console.log('statusCode: ', request && request.statusCode)
-            reject()
+
+            if (request) console.log('statusCode: ', request.statusCode)
+
+            reject(error)
           }
 
           resolve(body)
