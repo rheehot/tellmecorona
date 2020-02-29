42f33171077f1708501e107169f3660b33cc4d15
lark <121there@gmail.com>
2020-02-29 15:52:12 +0900

    비동기를 동기로 변경하면서 구조 변경

--- src/index.ts
+++ src/index.ts
@@ -9,62 +9,58 @@ const parser = new (require('./parser'))()
 
 Bot.sendStartMessage()
 
-Requester.load()
-  .then((data: any) => {
-    try {
-      let currentTimestamp: number = parser.getUpdateDate(data).getTime()
+let run = async () => {
+  try {
+    let html: string = await Requester.load()
+    let timestamp: number = parser.getUpdateDate(html).getTime()
+    let recentLog = await db.getRecentLog()
+    let recentTimestamp: number = new Date(recentLog.date).getTime()
 
-      db.getRecentLog((recentLog: any) => {
-        let lastTimestamp: number = new Date(recentLog.date).getTime()
+    // 업데이트날짜가 최신임
+    if (timestamp === recentTimestamp) return
 
-        console.log(currentTimestamp, lastTimestamp)
+    let currentDate: any = moment(timestamp)
+    let timeMessage: string =
+      currentDate.format('YYYY년 MM월 DD일 A hh시') + '\n'
+    let status: object[] | null = parser.getStatus(html)
 
-        if (currentTimestamp !== lastTimestamp) {
-          let currentDate: any = moment(currentTimestamp)
-          let timeMessage: string =
-            currentDate.format('YYYY년 MM월 DD일 A hh시') + '\n'
-          let status: object[] | null = parser.getStatus(data)
+    if (status === null) throw '확진자 파싱 실패'
 
-          if (status !== null) {
-            let statusMessage: string = status
-              .map((item: any) => {
-                let result: string =
-                  item.data.title + ' ' + item.data.displayValue + '명'
-                let increment: number = item.data.value - recentLog[item.key]
+    let statusMessage: string = status
+      .map((item: any) => {
+        let result: string =
+          item.data.title + ' ' + item.data.displayValue + '명'
+        let increment: number = item.data.value - recentLog[item.key]
 
-                if (increment > 0) {
-                  result += `(+${increment})`
-                }
-
-                return result
-              })
-              .join('\n')
-
-            let infectedItem: any = _.find(status, { key: 'infected' })
-            let testedItem: any = _.find(status, { key: 'tested' })
-            let recoveredItem: any = _.find(status, { key: 'recovered' })
-            let deathsItem: any = _.find(status, { key: 'deaths' })
-
-            db.addLog(
-              currentDate.format('YYYY-MM-DD HH:mm:ss'),
-              infectedItem.data.value,
-              recoveredItem.data.value,
-              deathsItem.data.value,
-              testedItem.data.value
-            )
-
-            console.log(timeMessage + statusMessage)
-            Bot.sendMessage(timeMessage + statusMessage)
-          }
+        if (increment > 0) {
+          result += `(+${increment})`
         }
 
-        db.end()
+        return result
       })
-    } catch (e) {
-      console.error(e)
-      Bot.sendMessageAdmin('ERROR: ' + e)
-    }
-  })
-  .catch((error: any) => {
-    Bot.sendMessageAdmin('ERROR: ' + error.message + '(path: Requester.load)')
-  })
+      .join('\n')
+
+    let infectedItem: any = _.find(status, { key: 'infected' })
+    let testedItem: any = _.find(status, { key: 'tested' })
+    let recoveredItem: any = _.find(status, { key: 'recovered' })
+    let deathsItem: any = _.find(status, { key: 'deaths' })
+
+    db.addLog(
+      currentDate.format('YYYY-MM-DD HH:mm:ss'),
+      infectedItem.data.value,
+      recoveredItem.data.value,
+      deathsItem.data.value,
+      testedItem.data.value
+    )
+
+    console.log(timeMessage + statusMessage)
+    Bot.sendMessage(timeMessage + statusMessage)
+  } catch (error) {
+    console.error(error)
+    Bot.sendMessageAdmin('ERROR: ' + error)
+  } finally {
+    db.end()
+  }
+}
+
+run()
