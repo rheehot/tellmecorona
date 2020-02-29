a5c31427fc2e19cc79d9c78ebd29105a66106f46
lark <121there@gmail.com>
2020-02-29 20:08:02 +0900

    Update types

--- src/requester.ts
+++ src/requester.ts
@@ -1,13 +1,12 @@
+import request from 'request'
 ;(() => {
-  const request = require('request')
-
   class Requester {
     public load(): Promise<string> {
       const url: string =
         'https://m.search.naver.com/search.naver?ie=utf8&query=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%99%95%EC%A7%84%EC%9E%90'
 
       return new Promise((resolve, reject) => {
-        request(url, (error: any, request: any, body: any) => {
+        request(url, (error: any, response: request.Response, body: any) => {
           if (error) reject(error)
 
           resolve(body)
