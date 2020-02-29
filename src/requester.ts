90aac36c0460b2953fda783d198c14bdc9923cb8
lark <121there@gmail.com>
2020-02-29 15:50:38 +0900

    에러처리 수정

--- src/requester.ts
+++ src/requester.ts
@@ -2,19 +2,13 @@
   const request = require('request')
 
   class Requester {
-    public load(): Promise<PromiseConstructor> {
+    public load(): Promise<string> {
       const url: string =
         'https://m.search.naver.com/search.naver?ie=utf8&query=%EC%BD%94%EB%A1%9C%EB%82%98+%ED%99%95%EC%A7%84%EC%9E%90'
 
       return new Promise((resolve, reject) => {
         request(url, (error: any, request: any, body: any) => {
-          if (error) {
-            console.error('Failed: ', error)
-
-            if (request) console.log('statusCode: ', request.statusCode)
-
-            reject(error)
-          }
+          if (error) reject(error)
 
           resolve(body)
         })
