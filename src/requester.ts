5582ca0e88d5ff73dca5f9d5e53137ba5542260b
lark <121there@gmail.com>
2020-03-02 23:24:31 +0900

    지역정보 request 추가

--- src/requester.ts
+++ src/requester.ts
@@ -1,4 +1,19 @@
 import request from 'request'
+
+export interface ApiRegion {
+  count: string
+  title: string
+  rate: string
+  url: string
+}
+
+export interface ApiRegionObject {
+  result: {
+    regions: Array<ApiRegion>
+    updateTime: string
+  }
+}
+
 export default new (class Requester {
   public load(): Promise<string> {
     const url: string =
@@ -9,6 +24,19 @@ export default new (class Requester {
         if (error) reject(error)
 
         resolve(body)
+      })
+    })
+  }
+
+  public loadRegion(): Promise<ApiRegionObject> {
+    const url: string =
+      'https://m.search.naver.com/p/csearch/content/nqapirender.nhn?where=m&pkid=9005&key=regionAPI&sort=sort_1&direction=desc&u1=13867393'
+
+    return new Promise((resolve, reject) => {
+      request(url, (error: any, response: request.Response, body: any) => {
+        if (error) reject(error)
+
+        resolve(JSON.parse(body))
       })
     })
   }
